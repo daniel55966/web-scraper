34d849a660d7c7c2457beba30211cdd499158aae
daniel55966 <daniel55966@gmail.com>
2025-05-13 14:52:06 -0600

    Web Scraper of Airbnb for learning purposes only.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-blue-500 w-full py-4 text-white text-center">
+      <p>
+        &copy; 2025 Airbnb Web Scraper by{" "}
+        <a href="https://danielanderson.site">Daniel Anderson</a>{" "}
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
